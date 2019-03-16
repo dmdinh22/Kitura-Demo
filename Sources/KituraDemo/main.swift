e44a535e4b22591738466e75e8fcf98672cb739d
David Dinh <dmdinh22@gmail.com>
2019-03-16 01:30:13 -0700

    add logging

--- Sources/KituraDemo/main.swift
+++ Sources/KituraDemo/main.swift
@@ -1,4 +1,13 @@
 import Kitura
+import HeliumLogger
+import LoggerAPI
+
+// custom logging
+let helium = HeliumLogger(.verbose)
+Log.logger = helium
+
+//// no custom logging
+//HeliumLogger.use()
 
 let router = Router()
 
@@ -6,14 +15,25 @@ router.get("/") { (request, response, next) in
     defer {
         next()
     }
+    Log.info("Custom log - about to send a Hello World response to the user.")
+    Log.verbose("Things are going just fine.")
+
     response.send("Hello world!\n")
 }
 
 router.get("/") { request, response, next in
+    Log.warning("Something looks fishy!")
+    Log.error("OH NO!")
+
     response.send("And hello again!\n")
+
     next()
 }
 
 // start server on provided port using router instance
 Kitura.addHTTPServer(onPort: 8080, with: router)
+
+// FastCGI app config
+Kitura.addFastCGIServer(onPort: 9000, with: router)
+
 Kitura.run()
