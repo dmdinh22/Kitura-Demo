384df0e7da577acbfb3a45e83cfab5e83193820c
David Dinh <dmdinh22@gmail.com>
2019-03-16 01:17:09 -0700

    understanding next and defer

--- Sources/KituraDemo/main.swift
+++ Sources/KituraDemo/main.swift
@@ -2,10 +2,18 @@ import Kitura
 
 let router = Router()
 
-router.get("/") { request, response, next in
+router.get("/") { (request, response, next) in
+    defer {
+        next()
+    }
     response.send("Hello world!\n")
+}
+
+router.get("/") { request, response, next in
+    response.send("And hello again!\n")
     next()
 }
 
+// start server on provided port using router instance
 Kitura.addHTTPServer(onPort: 8080, with: router)
 Kitura.run()
