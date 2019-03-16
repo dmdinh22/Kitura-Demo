b6fc37be905469aff952a77b44b204f49926ffdc
David Dinh <dmdinh22@gmail.com>
2019-03-16 00:42:30 -0700

    add dependencies and ran test route

--- Sources/KituraDemo/main.swift
+++ Sources/KituraDemo/main.swift
@@ -1,1 +1,11 @@
-print("Hello, world!")
+import Kitura
+
+let router = Router()
+
+router.get("/") { request, response, next in
+    response.send("Hello world!\n")
+    next()
+}
+
+Kitura.addHTTPServer(onPort: 8080, with: router)
+Kitura.run()
